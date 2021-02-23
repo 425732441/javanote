235b1ebbe2425a85f890982d11e3527264711b33
Zhanghualei <zhanghualei@bonc.com.cn>
2021-02-23 10:28:25 +0800

    init

---
+++ src/main/java/com/zhl/javanote/JavanoteApplication.java
@@ -0,0 +1,13 @@
+package com.zhl.javanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavanoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhl/javanote/JavanoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhl.javanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
