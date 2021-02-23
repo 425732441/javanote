d9a0655900696deff5cca3fa6d08bfb0c4b39c99
Zhanghualei <zhanghualei@bonc.com.cn>
2021-02-23 14:00:16 +0800

    nio 测试

---
+++ src/main/java/com/zhl/javanote/io/nio/Server.java
@@ -0,0 +1,26 @@
+package com.zhl.javanote.io.nio;
+
+/**
+ * @author Zhanghualei
+ * @Classname a
+ * @Date 2021/2/23 11:33
+ */
+public class Server {
+    private static int DEFAULT_PORT = 12345;
+    private static ServerHandle serverHandle;
+
+    public static void start() {
+        start(DEFAULT_PORT);
+    }
+
+    public static synchronized void start(int port) {
+        if (serverHandle != null)
+            serverHandle.stop();
+        serverHandle = new ServerHandle(port);
+        new Thread(serverHandle, "Server").start();
+    }
+
+    public static void main(String[] args) {
+        start();
+    }
+}
